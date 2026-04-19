dc1d78991fa1df5eff63d0562473d2fa210d2f05
MOMOE666 <roseannalach@gmail.com>
2026-04-19 17:25:38 +0200

    worek
    worek

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -1,4 +1,4 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,7 +21,7 @@ public class Interactor : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-         
+
             CheckInteracte();
         }
     }
@@ -29,20 +29,20 @@ public class Interactor : MonoBehaviour
     private void CheckInteracte()
     {
         // Start a new interaction
-    
+
         Collider[] colliders = Physics.OverlapSphere(InteractorSource.position, InteractRange, interactableLayer);
         foreach (Collider collider in colliders)
         {
-          
+
             if (collider.TryGetComponent<IInteractable>(out var interactObj))
             {
-               
+
                 // Check if the interactable object is within the specified range
                 float distance = Vector3.Distance(InteractorSource.position, collider.transform.position);
-                if (distance <= InteractRange &&  !inventory.Contains(interactObj))
+                if (distance <= InteractRange )
                 {
-                    
-                    interactObj.Interact();  
+
+                    interactObj.Interact();
                     inventory.Add(interactObj);
                 }
             }
